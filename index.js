3ff29220470785c362b9c1395c90910017d24355
Satyam Kumar <satyam2002kumar@gmail.com>
2026-01-17 01:37:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require('express');
+const dotenv=require('dotenv');
+dotenv.config();
 const app=express();
 
 app.get('/home',(req,res)=>{
-    res.json({message:'OK'});
+    res.json({message:'OK bbyee'});
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 3000");
 });
